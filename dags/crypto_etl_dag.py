a96228630aca5437be1e8ddad0d12c546a8997c2
Sergey Samarin <sergei.sam.92@gmail.com>
2026-07-11 15:09:41 +0300

    Add crypto_etl_dag.py: Airflow DAG for crypto-etl-project

---
+++ dags/crypto_etl_dag.py
@@ -0,0 +1,26 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+default_args = {
+    'owner': 'me',
+    'start_date': datetime(2026, 7, 9),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=2),
+}
+
+dag = DAG(
+    'crypto_etl_dag',
+    default_args=default_args,
+    description='Запуск crypto-etl-project через Docker Compose',
+    schedule_interval='*/10 * * * *',
+    catchup=False,
+)
+
+run_crypto_etl = BashOperator(
+    task_id='run_crypto_etl',
+    bash_command='cd /mnt/d/work/crypto-etl-project && docker-compose up --exit-code-from crypto-etl',
+    dag=dag,
+)
+
+run_crypto_etl
