dc712448ba63e09908756825ad994486f8146097
Sergey Samarin <sergei.sam.92@gmail.com>
2026-07-23 18:54:52 +0300

    Add python two_sum.py

---
+++ python/two_sum.py
@@ -0,0 +1,11 @@
+def two_sum(nums, target):
+    seen = {} # здесь будет хеш-таблица(словарь)
+    for i, num in emumerate(nums):
+        complement = target - num
+        # проверрка в словаре происходит Мгновенно (О(1)), без перебора!
+        if complement in seen:
+            return [seen[complement], i]
+        seen[num] = i
+
+# проверить здесь же:
+print(two_sum([2, 7, 11, 15], 9)) # [0, 1]
